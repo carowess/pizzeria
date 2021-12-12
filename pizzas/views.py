1bce71d368cfed8d948473f68b690b507771066a
Caroline Wescott <caroline_wescott1@baylor.edu>
2021-12-12 16:19:12 -0600

    Added specific pizza page

--- pizzas/views.py
+++ pizzas/views.py
@@ -14,3 +14,13 @@ def pizzas(request):
     context = {'pizzas':pizzas}
 
     return render(request, 'pizzas/pizzas.html', context)
+
+
+def pizza(request,pizza_id):
+    pizza = Pizza.objects.get(id=pizza_id)
+
+    toppings = pizza.topping_set.all()
+
+    context = {'pizza':pizza, 'toppings':toppings}
+
+    return render(request, 'pizzas/pizza.html', context)
